f585f98b7601590c35bca7c28446898b7b88c37d
xqli <xianqiu.li26@gmail.com>
2024-03-08 07:06:38 +0000

    main download push

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 #-*- coding:utf-8 -*-
 
 
+import os
+import sys
+import time
 import pandas as pd
+
 from binance.futures import CoinM
+from datetime import datetime as dt
+from datetime import timedelta as td
 
 
 # global
-symbol = 'DOGEUSD_PERP'
-stop_s = '2024-01-01'
-start_s = '2023-07-01'
+symbol =  sys.argv[1] # 'DOGEUSD_PERP'
+stop_s = '2024-03-05'
+start_s = '2023-01-21'
 
 # ED25519 Keys
 api_key = "../api_key.txt"
 private_key = "../private_key.pem"
 private_key_pass = ""
-
+day_ms_count = 24 * 3600 * 1000
 stop_t = int(pd.Timestamp(stop_s).timestamp() * 1000)
 start_t = int(pd.Timestamp(start_s).timestamp() * 1000)
 
@@ -34,16 +40,22 @@ client = CoinM(
 )
 
 # Get server timestamp
-print(client.time())
+# print(client.time())
 # Get klines of BTCUSDT at 1m interval
-df = pd.DataFrame(
-    client.klines(symbol, "1m", startTime=start_t, endTime=stop_t),
-    columns=[
-        'start_t', 'open', 'high', 'low', 'close',
-        'volume', 'end_t', 'amount', 'trade_cnt',
-        'taker_vol', 'taker_amt', 'reserved'
-    ])
-df.to_csv(f'{symbol}_{start_s}_{stop_s}.csv', index=False)
+os.makedirs(f'data/{symbol}/', exist_ok=True)
+while start_t < stop_t:
+    day = pd.to_datetime(start_t * 1e6)
+    start_t += day_ms_count
+    df = pd.DataFrame(
+        client.klines(symbol, "1m", endTime=start_t, limit=1441)[:-1],
+        columns=[
+            'start_t', 'open', 'high', 'low', 'close',
+            'volume', 'end_t', 'amount', 'trade_cnt',
+            'taker_vol', 'taker_amt', 'reserved'
+        ])
+    df.to_csv(f'data/{symbol}/{day.date()}.csv', index=False)
+    print(symbol, day)
+    time.sleep(1/40)
 #print(client.ext_klines("BTCUSD", "1m", 'continuous', contractType='PERPETUAL', limit=1))
 #print(client.ext_klines("BTCUSD", "1m", 'indexPrice', limit=1))
 ## Get last 10 klines of BNBUSDT at 1h interval
