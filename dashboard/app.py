ec8e80e7681ccf107aef089e1067790d70c92536
xqli <xianqiu_li@163.com>
2024-05-05 06:38:33 +0000

    more information added to dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -28,22 +28,87 @@ div.stButton>button {
 </style>'''
 
 
-def trade_info():
+def trade_info(cli, symbol, start_time, stgname=None):
+    orders = cli.get_orders(
+        symbol=symbol, limit=100)
+    end_time = orders[0]['time']
+    start_time = (start_time.timestamp() - 8 * 3600) * 1000 # to utc
+    while end_time >= start_time:
+        order_t = cli.get_orders(
+            symbol=symbol, limit=100, endTime=end_time)
+        if len(order_t) == 1: # not more orders any more
+            break
+        end_time = order_t[0]['time']
+        orders = order_t[:-1] + orders
+    orders = pd.DataFrame(orders)
+    slc_fields = ["time", "status", "price", "avgPrice", "side",
+        "span", "executedQty"]
+    if stgname:
+        stgname = f'{stgname}_'
+        orders = orders[orders['clientOrderId'].str.startswith(stgname)] 
+    orders['status'] = orders.pop('clientOrderId').apply(
+        lambda x: x.split('_')[-2].upper())
+    orders.time = pd.to_datetime((orders.time + 8 * 3600000) * 1e6)
+    orders['span'] = orders['price'].astype(float).diff()
+    orders = orders[orders.status != 'OPEN']
+    orders['span'] *= orders['side'].apply(lambda x: -1 if x == 'BUY' else 1)
+    return orders[slc_fields]
+
+
+def account_info():
     api_key, private_key = get_auth_keys()
     client = CoinM(
         api_key=api_key,
         private_key=private_key)
-    result = []
-    for symbol in COIN_LIST:
-        symbol = f'{symbol}USD_PERP'
-        rsp = client.ticker_price(symbol)[0]
-        result.append([symbol, rsp['price']]) 
+    # orderbook info
     orders = client.get_open_orders()
     orders = pd.DataFrame(orders)[[
         'time', 'pair', 'side', 'price', 'status', 'clientOrderId', 'origQty']]
     orders.time = pd.to_datetime((orders.time+ 8 * 3600000) * 1e6)
-    result = pd.DataFrame(result, columns=['symbol', 'price'])
-    return orders, result
+    orders = orders.sort_values(['pair', 'time'])
+    # account info
+    acc_info = client.account()
+    ## asset info
+    assets = pd.DataFrame(acc_info['assets'])[
+        ['asset', 'walletBalance', 'unrealizedProfit']]
+    assets = assets[assets["walletBalance"].astype(float) != 0]
+    assets = assets.sort_values("asset")
+    price_cache, prices = {}, []
+    for symbol in assets['asset']:
+        symbol = f'{symbol}USD_PERP'
+        rsp = client.ticker_price(symbol)[0]
+        prices.append(float(rsp['price']))
+        price_cache[symbol] = prices[-1]
+    total = 0
+    for field in ["walletBalance", "unrealizedProfit"]:
+        assets[field] = assets[field].astype(float) * prices
+        total += assets[field]
+    assets['total'] = total
+    ## postion info
+    pos_fields = [
+        'symbol', 'initialMargin', 'positionAmt', 'entryPrice', 'unrealizedProfit',
+        'leverage', 'maxQty']
+    positions = pd.DataFrame(acc_info['positions'])[pos_fields]
+    positions = positions.sort_values("symbol")
+    if not positions.empty:
+        positions = positions[positions['positionAmt'] != '0']
+        # market info
+        result = []
+        for symbol in positions['symbol']:
+            if symbol in price_cache:
+                price = price_cache[symbol]
+            else:
+                rsp = client.ticker_price(symbol)[0]
+                price = float(rsp['price'])
+            result.append(price)
+        positions['lastPrice'] = result
+        for field in ['initialMargin', 'entryPrice', 'unrealizedProfit']:
+            positions[field] = positions[field].astype(float)
+        positions['unrealizedProfit'] /= positions['initialMargin']
+        positions['initialMargin'] *= positions['lastPrice']
+        pos_fields.insert(2, 'lastPrice')
+        positions = positions[pos_fields]
+    return client, assets, positions, orders
     
 
 def overview(ratio):
@@ -124,7 +189,7 @@ def log_parser(filename, ratio):
     df['side'] = df.direct.apply(lambda x: 1 if x == 'LONG' else -1)
     df['gross'] = df.side * (df.close - df.open)
     df['commis'] = (df.close + df.open) * ratio
-    df.index = pd.to_datetime(df.pop('st').astype(float) * 1e6)
+    df.index = pd.to_datetime((df.pop('st').astype(float) + 8 * 3600000) * 1e6)
     return df
 
 
@@ -155,25 +220,6 @@ def main():
     df = process_info()
     st.table(df)
     default = "请选择..."
-    # cols = st.columns([3, 3, 3, 1, 1])
-    # with cols[0]:
-    #     ratio = st.selectbox(
-    #         '手续费率：', options=[0.0002, 0.0005])
-    # with cols[1]:
-    #     symbol = st.selectbox(
-    #         "币种：", options=[default, "BTC", "BNB", "DOGE"])
-    # with cols[2]:
-    #     if symbol in {default}:
-    #         symbol = "*"
-    #     else:
-    #         symbol = symbol.lower()
-    #     filename = st.selectbox(
-    #         '日志文件：', options=[default] + sorted(
-    #             glob.glob(f"./{symbol}-*.log")))
-    # with cols[3]:
-    #     st.write("")
-    #     st.write("")
-    #     overview = st.button("查看总览")
     with st.sidebar:
         ratio = st.selectbox(
             '手续费率：', options=[0.0002, 0.0005])
@@ -194,18 +240,25 @@ def main():
         df = overview(ratio) 
         st.table(df)
     # trade info
-    open_orders, market_info = trade_info()
-    st.markdown("## 行情信息")
-    st.table(market_info)
+    cli, asset_info, pos_info, open_orders = account_info()
+    st.markdown('## 资产列表')
+    st.table(asset_info)
+    st.markdown("## 持仓信息")
+    st.table(pos_info)
     if st.button("刷新", key='refresh2'):
         st.rerun()
     st.markdown("## 挂单信息")
     st.table(open_orders)
     if filename != default:
         table = log_parser(filename, ratio)
+        trans = trade_info(
+            cli, f'{symbol.upper()}USD_PERP', table.index[0],
+            os.path.basename(filename)[:-4])
+        st.markdown("## 真实交易记录")
+        st.table(trans)
         summary = log_summary(table)
         st.markdown(summary)
-        st.markdown("### All Trade History")
+        st.markdown("## All Trade History")
         st.table(table)
     
 
