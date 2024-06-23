7666a9c5b71e6cd0fc274ff4c9bce09a8bbce9c4
xqli <xianqiu_li@163.com>
2024-06-23 19:03:12 +0800

    update

--- binance/auth/utils.py
+++ binance/auth/utils.py
@@ -4,19 +4,31 @@
 
 import os
 import logging
+import streamlit as st
 import subprocess as sb
 
 
-def get_auth_keys(prefix='strategy'):
+def get_auth_keys(src='env'):
     # ED25519 Keys
-    api_key = f"{prefix}/api_key.txt"
-    with open(api_key) as f:
-        api_key = f.read().strip()
-
-    private_key = f"{prefix}/private_key.pem"
-    with open(private_key, 'rb') as f:
-        private_key = f.read().strip()
-    return api_key, private_key
+    stt_lin = "-----BEGIN PRIVATE KEY-----"
+    end_lin = "-----END PRIVATE KEY-----"
+    if src == 'file':
+        prefix = 'config'
+        api_key = f"{prefix}/api_key.txt"
+        with open(api_key) as f:
+            api_key = f.read().strip()
+        prv_key = f"{prefix}/private_key.pem"
+        with open(private_key, 'rb') as f:
+            private_key = f.read().strip()
+    elif src == 'env':
+        api_key = os.environ.get('API_KEY')
+        prv_key = os.environ.get('PRV_KEY')
+        prv_key = f'{stt_lin}\n{prv_key}\n{end_lin}'
+    elif src == 'secret':
+        api_key = st.secrets['API_KEY']
+        prv_key = st.sectets['PRV_KEY']
+        prv_key = f'{stt_lin}\n{prv_key}\n{end_lin}'
+    return api_key, prv_key
 
 
 def on_open(self):
